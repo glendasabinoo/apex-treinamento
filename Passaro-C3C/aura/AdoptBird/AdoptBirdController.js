46770684b05408ca384d76b49570a7c0596f1af7
Glenda Sabino <95448881+glendasabinoo@users.noreply.github.com>
2022-03-29 11:29:29 -0300

    Add files via upload

--- Passaro-C3C/aura/AdoptBird/AdoptBirdController.js
+++ Passaro-C3C/aura/AdoptBird/AdoptBirdController.js
@@ -1,15 +1,15 @@
 ({
     adoptBird : function(component, event, helper) {
-        let action = component.get('c.randomBird');
+        let action = component.get('c.createBird');
 
         action.setCallback(this, function (response){
-            let state = response.getState();
+           	let state = response.getState();
             let result = response.getReturnValue();
 
             component.set('v.names', result[0]);
             component.set('v.breeds', result[1]);
-            component.set('v.photos', result[2]);
-            component.set('v.colors', result[3]);
+            component.set('v.colors', result[2]);
+            component.set('v.photos', result[3]);
         })
         
         $A.enqueueAction(action);
