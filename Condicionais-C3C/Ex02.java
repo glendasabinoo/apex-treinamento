afb72da26973dbbb249ae6147f4eed339c1a2a5f
Glenda Sabino <95448881+glendasabinoo@users.noreply.github.com>
2022-04-04 19:17:09 -0300

    Rename Ex02.java to Condicionais-C3C/Ex02.java

--- Condicionais-C3C/Ex02.java
+++ Condicionais-C3C/Ex02.java
@@ -1,48 +1,48 @@
 package treinamentoc3c;
 import java.util.Scanner;
-//Receber 3 valores e armazena-los em ordem crescente em um array usando somente estruturas de condição.
+//Receber 3 valores e armazena-los em ordem crescente em um array usando somente estruturas de condiÃ§Ã£o.
 public class Ex02 {
 
 	public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
-        System.out.println("Entre com primeiro número:");
+        System.out.println("Entre com primeiro nÃºmero:");
         int num1 = scan.nextInt();
         
-        System.out.println("Entre com segundo número:");
+        System.out.println("Entre com segundo nÃºmero:");
         int num2 = scan.nextInt();
         
-        System.out.println("Entre com terceiro número:");
+        System.out.println("Entre com terceiro nÃºmero:");
         int num3 = scan.nextInt();
         
         if (num1 <= num2 && num1 <= num3 && num2 <= num3){
-            //num1 é menor
-            //num3 é maior
+            //num1 Ã© menor
+            //num3 Ã© maior
             // num1 < num2 < num3
             System.out.println(num1 + " - " + num2 + " - " + num3);
         } else if (num1 <= num2 && num1 <= num3 && num3 <= num2){
-            //num1 é menor
-            //num2 é maior
+            //num1 Ã© menor
+            //num2 Ã© maior
             // num1 < num3 < num2
             System.out.println(num1 + " - " + num3 + " - " + num2);
         } else if (num2 <= num1 && num2 <= num3 && num1 <= num3){
-            //num2 é menor
-            //num3 é maior
+            //num2 Ã© menor
+            //num3 Ã© maior
             // num2 < num1 < num3
             System.out.println(num2 + " - " + num1 + " - " + num3);
         } else if (num2 <= num1 && num2 <= num3 && num3 <= num1){
-            //num2 é menor
-            //num1 é maior
+            //num2 Ã© menor
+            //num1 Ã© maior
             // num2 < num3 < num1
             System.out.println(num2 + " - " + num3 + " - " + num1);
         } else if (num3 <= num1 && num3 <= num2 && num1 <= num2){
-            //num3 é menor
-            //num2 é maior
+            //num3 Ã© menor
+            //num2 Ã© maior
             // num3 < num1 < num2
             System.out.println(num3 + " - " + num1 + " - " + num2);    
         } else {
-            //num3 é menor
-            //num1 é maior
+            //num3 Ã© menor
+            //num1 Ã© maior
             // num3 < num2 < num1
             System.out.println(num3 + " - " + num2 + " - " + num1);  
         }
